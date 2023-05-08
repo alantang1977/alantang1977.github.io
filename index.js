82042ef26f90ef36aa1d149435faf55225a2d412
超人哥哥 <107459091+alantang1977@users.noreply.github.com>
2023-05-08 13:23:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, welcome Alan :)'
+  document.getElementById('main-content').innerHTML = 'Hello, welcome Alan网站，想要TVBox接口请浏览https://github.com/alantang1977:)'
 }
